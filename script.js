d2ceae4103a2e3c332f96cc27060e7d7d5c525cc
Aydyn <101376168+aydyn-lotta@users.noreply.github.com>
2023-06-15 17:20:10 +0500

    Update script.js

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ function GetBody() {
 };
 
 function sendEmail() {
-  var email = 'someone@gmail.com';
-  var body = GetBody();
+  var email = 'aydynlotta18@gmail.com';
+  var body = "отправьте нам это письмо, ваш комментарий скоро будет на сайте!" + GetBody();
   var subject = document.getElementById('subject').value;
   var mailto_link = 'mailto:' + email + '?subject=' + subject + '&body=' + encodeURIComponent(body);
   window.location.href = mailto_link;
