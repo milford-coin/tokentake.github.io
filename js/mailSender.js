4341a2d362a4e21ee279b494bac33d7ef0915981
Aydyn <101376168+aydyn-lotta@users.noreply.github.com>
2023-06-16 10:39:01 +0500

    Create mailSender.js

---
+++ js/mailSender.js
@@ -0,0 +1,6 @@
+function mailTo()
+{
+	var subject = "Комментарий"
+	var body = document,getElementById('message').value;
+	window.location.href = "aydyn-lotta@hotmail.com?subject" + subject + "&body" + body;
+}
